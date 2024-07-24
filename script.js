e3fa9b0aa90abae9604ed0deea7dee7aaae0ef8f
mairadossantos <mairaprimeiro@gmail.com>
2024-07-24 14:50:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nota1 = prompt("qual a sua nota?");
+let nota2 = prompt("qual a sua nota?");
+let nota3 = prompt("qual a sua nota?");
+
+let notas = [nota1, nota2, nota3];
+let soma = notas[0] + notas1[1] +[2];
+let media = soma / notas.length;
+
+console.log("A média da nota é:" + media);
